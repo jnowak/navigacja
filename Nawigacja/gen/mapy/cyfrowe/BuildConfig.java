630b322e736ff68250825cab3710db316ef34dee
Dell <Dell@Dell-Komputer>
2012-03-24 20:30:59 +0100

    navi update

---
+++ Nawigacja/gen/mapy/cyfrowe/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package mapy.cyfrowe;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- Nawigacja/src/mapy/cyfrowe/NawigacjaActivity.java
+++ Nawigacja/src/mapy/cyfrowe/NawigacjaActivity.java
@@ -2,12 +2,34 @@ package mapy.cyfrowe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MapView.LayoutParams;  
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class NawigacjaActivity extends Activity {
+
+
+public class NawigacjaActivity extends MapActivity {
+	MapView mapView;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        LinearLayout zoomLayout = (LinearLayout)findViewById(R.id.zoom);  
+        View zoomView = mapView.getZoomControls(); 
+ 
+        zoomLayout.addView(zoomView, 
+            new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, 
+                LayoutParams.WRAP_CONTENT)); 
+        mapView.displayZoomControls(true);
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
