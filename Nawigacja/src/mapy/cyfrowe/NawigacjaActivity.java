8e27ceb8113c5aec16c3092939976ffce9a39fdf
Dell <Dell@Dell-Komputer>
2012-03-24 21:46:10 +0100

    added pushpin

--- Nawigacja/src/mapy/cyfrowe/NawigacjaActivity.java
+++ Nawigacja/src/mapy/cyfrowe/NawigacjaActivity.java
@@ -1,18 +1,96 @@
 package mapy.cyfrowe;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
+
+
 import android.os.Bundle;
-import android.widget.TextView;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapView;
-import com.google.android.maps.MapView.LayoutParams;  
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.MapView.LayoutParams;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+
 
 
 
 public class NawigacjaActivity extends MapActivity {
 	MapView mapView;
+    MapController mc;
+    GeoPoint p;
+    
+    
+    class MapOverlay extends com.google.android.maps.Overlay
+    {
+        @Override
+        public boolean draw(Canvas canvas, MapView mapView, 
+        boolean shadow, long when) 
+        {
+            super.draw(canvas, mapView, shadow);                   
+ 
+            //---translate the GeoPoint to screen pixels---
+            Point screenPts = new Point();
+            mapView.getProjection().toPixels(p, screenPts);
+ 
+            //---add the marker---
+            Bitmap bmp = BitmapFactory.decodeResource(
+                getResources(), R.drawable.pushpin);            
+            canvas.drawBitmap(bmp, screenPts.x, screenPts.y-50, null);         
+            return true;
+        }
+        @Override
+        public boolean onTouchEvent(MotionEvent event, MapView mapView) 
+        {   
+            //---when user lifts his finger---
+            if (event.getAction() == 1) {                
+                GeoPoint p = mapView.getProjection().fromPixels(
+                    (int) event.getX(),
+                    (int) event.getY());
+ 
+                Geocoder geoCoder = new Geocoder(
+                    getBaseContext(), Locale.getDefault());
+                try {
+                    List<Address> addresses = geoCoder.getFromLocation(
+                        p.getLatitudeE6()  / 1E6, 
+                        p.getLongitudeE6() / 1E6, 1);
+ 
+                    String add = "";
+                    if (addresses.size() > 0) 
+                    {
+                        for (int i=0; i<addresses.get(0).getMaxAddressLineIndex(); 
+                             i++)
+                           add += addresses.get(0).getAddressLine(i) + "\n";
+                    }
+ 
+                    Toast.makeText(getBaseContext(), add, Toast.LENGTH_SHORT).show();
+                }
+                catch (IOException e) {                
+                    e.printStackTrace();
+                }   
+                return true;
+            }
+            else                
+                return false;
+        }                
+    } 
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +105,24 @@ public class NawigacjaActivity extends MapActivity {
                 LayoutParams.WRAP_CONTENT, 
                 LayoutParams.WRAP_CONTENT)); 
         mapView.displayZoomControls(true);
+        mapView.setStreetView(true);
+        mc = mapView.getController();
+        String coordinates[] = {"54.371673","18.612381"};
+        double lat = Double.parseDouble(coordinates[0]);
+        double lng = Double.parseDouble(coordinates[1]);
+ 
+        p = new GeoPoint(
+            (int) (lat * 1E6), 
+            (int) (lng * 1E6));
+ 
+        mc.animateTo(p);
+        mc.setZoom(17); 
+        //---Add a location marker---
+       MapOverlay mapOverlay = new MapOverlay();
+        List<Overlay> listOfOverlays = mapView.getOverlays();
+        listOfOverlays.clear();
+        listOfOverlays.add(mapOverlay);
+        mapView.invalidate();
     }
     @Override
     protected boolean isRouteDisplayed() {
