b6a7fbb7cd244e0da722fed3768ce6d8bf5efe50
jphn <jnowak_66@o2.pl>
2012-05-24 14:48:23 +0200

    poprawki w nawigacja activity

--- Nawigacja/src/mapy/cyfrowe/NawigacjaActivity.java
+++ Nawigacja/src/mapy/cyfrowe/NawigacjaActivity.java
@@ -1,6 +1,5 @@
 package mapy.cyfrowe;
 
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
@@ -13,7 +12,6 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-
 
 import android.os.Bundle;
 import android.util.Log;
@@ -38,127 +36,159 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
 
-
-
-
-public class NawigacjaActivity extends MapActivity {
+public class NawigacjaActivity extends MapActivity
+{
 	MapView mapView;
-    MapController mc;
-    GeoPoint p;
+	MapController mc;
+	GeoPoint p;
 	private Button addPOIButton;
 
-	
 	public static Location currLocation;
 	public static RouteOverlay route = new RouteOverlay();
-	
-	public List<Overlay> listOfOverlays;
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	
-    	
-    	Intent intend = getIntent();
-    	final MapOverlay mapOverlay = new MapOverlay();
-    	
-    	if (currLocation != null)
-    	{}
-    	String from=intend.getStringExtra("from");
-    	String to=intend.getStringExtra("to");
-    	String s = MapWebServices.getDistanceAndTime(from, to, TransportMode.driving);
-        Step[] steps = MapWebServices.getRoute(from, to, TransportMode.driving);
-    	
-        setContentView(R.layout.main);
-        mapView = (MapView) findViewById(R.id.mapView);
-        LinearLayout zoomLayout = (LinearLayout)findViewById(R.id.zoom);  
-        View zoomView = mapView.getZoomControls(); 
- 
-        zoomLayout.addView(zoomView, 
-            new LinearLayout.LayoutParams(
-                LayoutParams.WRAP_CONTENT, 
-                LayoutParams.WRAP_CONTENT)); 
-        mapView.displayZoomControls(true);
-        
-        
-        
-        LocationListener locationListener = new ConcreteLocationListener(this, mapOverlay, mapView);
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        
-        mc = mapView.getController();
-       
-        listOfOverlays = mapView.getOverlays();
-        listOfOverlays.clear();
-        listOfOverlays.add(mapOverlay);
-        route.setSteps(steps);
-        if (steps != null)
-        	listOfOverlays.add(route);
-        mapView.invalidate();
 
-        addPOIButton = (Button)findViewById(R.id.addPOI);
-        
-        addPOIButton.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View arg0) {
+	public List<Overlay> listOfOverlays;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		Intent intend = getIntent();
+		final MapOverlay mapOverlay = new MapOverlay();
+
+		if (currLocation != null)
+		{
+		}
+		Step[] steps = null;
+		String activity = intend.getStringExtra("activity");
+		if (activity != null)
+		{
+			if (activity.compareTo("FromTo") == 0)
+			{
+				String from = intend.getStringExtra("from");
+				String to = intend.getStringExtra("to");
+				String s = MapWebServices.getDistanceAndTime(from, to,
+						TransportMode.driving);
+				steps = MapWebServices
+						.getRoute(from, to, TransportMode.driving);
+			} else if (activity.compareTo("GoTo") == 0)
+			{
+				String to = intend.getStringExtra("to");
+				String tryb = intend.getStringExtra("tryb");
+				if (tryb.compareTo("samochod") == 0)
+				{
+					steps = MapWebServices
+							.getRoute(
+									Double.toString(NawigacjaActivity.currLocation
+											.getLatitude())
+											+ ","
+											+ Double.toString(NawigacjaActivity.currLocation
+													.getLongitude()), to,
+									TransportMode.driving);
+				} else if (tryb.compareTo("pieszo") == 0)
+				{
+					steps = MapWebServices
+							.getRoute(
+									Double.toString(NawigacjaActivity.currLocation
+											.getLatitude())
+											+ ","
+											+ Double.toString(NawigacjaActivity.currLocation
+													.getLongitude()), to,
+									TransportMode.walking);
+				}
+				NawigacjaActivity.route.setSteps(steps);
+			}
+		}
+		setContentView(R.layout.main);
+		mapView = (MapView) findViewById(R.id.mapView);
+		LinearLayout zoomLayout = (LinearLayout) findViewById(R.id.zoom);
+		View zoomView = mapView.getZoomControls();
+
+		zoomLayout.addView(zoomView, new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		mapView.displayZoomControls(true);
+
+		LocationListener locationListener = new ConcreteLocationListener(this,
+				mapOverlay, mapView);
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+				locationListener);
+
+		mc = mapView.getController();
+
+		listOfOverlays = mapView.getOverlays();
+		listOfOverlays.clear();
+		listOfOverlays.add(mapOverlay);
+		route.setSteps(steps);
+		if (steps != null)
+			listOfOverlays.add(route);
+		mapView.invalidate();
+
+		addPOIButton = (Button) findViewById(R.id.addPOI);
+
+		addPOIButton.setOnClickListener(new OnClickListener()
+		{
+
+			public void onClick(View arg0)
+			{
 				POIList.add(currLocation);
-				
+
 			}
 		});
-        
-        
-        
-        
-        
-        Button bt =(Button)findViewById(R.id.Button01);
-        bt.setOnClickListener(new OnClickListener() {
+
+		Button bt = (Button) findViewById(R.id.Button01);
+		bt.setOnClickListener(new OnClickListener()
+		{
 
 			@SuppressWarnings("deprecation")
-			public void onClick(View v) {
-				if(mapView.isStreetView()){
+			public void onClick(View v)
+			{
+				if (mapView.isStreetView())
+				{
 					mapView.setStreetView(false);
 					mapView.setSatellite(true);
-				}
-				else
+				} else
 				{
-					
+
 					mapView.setStreetView(true);
 					mapView.setSatellite(false);
-				
-				}
-				}
-        });
-        Button bt2 =(Button)findViewById(R.id.Button02);
-        bt2.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					if(mapView.isTraffic()){
-						mapView.setTraffic(false);
-					}
-					else
-					{
-						mapView.setTraffic(true);
-					
-					}
-					}
-			
-        });
-        Button bt3 =(Button)findViewById(R.id.Button03);
-        bt3.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					Intent intent = new Intent();
-		            setResult(RESULT_OK, intent);
-		            finish();
-					}
-			
-        });
 
+				}
+			}
+		});
+		Button bt2 = (Button) findViewById(R.id.Button02);
+		bt2.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				if (mapView.isTraffic())
+				{
+					mapView.setTraffic(false);
+				} else
+				{
+					mapView.setTraffic(true);
 
-				
-			
-        
-        
-    }
-    @Override
-    protected boolean isRouteDisplayed() {
-        return false;
-    }
+				}
+			}
+
+		});
+		Button bt3 = (Button) findViewById(R.id.Button03);
+		bt3.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				Intent intent = new Intent();
+				setResult(RESULT_OK, intent);
+				finish();
+			}
+
+		});
+
+	}
+
+	@Override
+	protected boolean isRouteDisplayed()
+	{
+		return false;
+	}
 }
